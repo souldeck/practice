5b6a54782ed8f419af02d8c4a68d438601f66ea1
souldeck <45283167+souldeck@users.noreply.github.com>
2019-05-21 23:24:17 +0900

    3. 메소드 calculate 따로 만들어 2,3,4단 구현하기

--- src/tj/practice.java
+++ src/tj/practice.java
@@ -1,28 +1,35 @@
 package tj;
 
+
+
 public class practice {
-	public static void main(String[] args) {
-	int[] result = new int[9];
-	for(int i=0; i < result.length; i++) {
-		result[i] = 2 * (i+1);
+	public static int[] calculate(int times) {
+		int[] result = new int[9];
+		for(int i=0; i < result.length; i++) {
+			result[i] = times * (i+1);
+		}
+		return result;
 	}
-	for(int i=0; i < result.length; i++) {
-		System.out.println(result[i]);
-	}
+
 	
-	int[] times3 = new int[9];
-	for(int i=0; i < times3.length; i++) {
-		times3[i] = 3 * (i+1);
+	public static void main(String[] args) {
+		int[] result = calculate(2);
+		
+		for(int i=0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+		int[] times3 = calculate(3);
+		
+		for(int i=0; i < times3.length; i++) {
+			System.out.println(times3[i]);
+		}
+		int[] times4 = calculate(4);
+		for(int i=0; i < times4.length; i++) {
+			System.out.println(times4[i]);
+		}
+		
+	
 	}
-	for(int i=0; i < times3.length; i++) {
-		System.out.println(times3[i]);
-	}
-	int[] times4 = new int[9];
-	for(int i=0; i < times4.length; i++) {
-		times4[i]=4*(i+1);
-	}
-	for(int i=0; i < times4.length; i++) {
-		System.out.println(times4[i]);
-	}
+
+	
 }
-}	
